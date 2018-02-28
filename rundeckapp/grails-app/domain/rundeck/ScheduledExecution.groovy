284cbd890440ba10872464ee54b01b4184ccbcc1
Stephen Joyner <stephen@Stephens-MacBook-Pro.local>
2018-02-28 15:54:45 -0600

    Fix roles loaded from container sources. Fix interceptors. Add/fix assets. Change datasource def method so external property override will work.

--- rundeckapp/grails-app/domain/rundeck/ScheduledExecution.groovy
+++ rundeckapp/grails-app/domain/rundeck/ScheduledExecution.groovy
@@ -182,7 +182,7 @@ class ScheduledExecution extends ExecutionContext {
     }
 
     static namedQueries = {
-		isScheduled {
+		scheduledJobs {
 			eq 'scheduled', true
 		}
 		withServerUUID { uuid ->
